c2b06c90ca6812c7449bd4c790869ff84573d6e7
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-02-27 17:11:52 +0000

    Add a script to create a much more granular graph

--- sort.pl
+++ sort.pl
@@ -21,6 +21,7 @@ sub numberOfDays
 } 
 
 my $dir = shift // '.';
+my $total = 0;
  
 opendir my $dh, $dir or die "Could not open '$dir' for reading '$!'\n";
 my @things = grep {$_ ne '.' and $_ ne '..' and /^walk.*\.txt$/} readdir $dh;
@@ -28,14 +29,21 @@ my @things = grep {$_ ne '.' and $_ ne '..' and /^walk.*\.txt$/} readdir $dh;
 @things = sort {numberOfDays($a) <=> numberOfDays($b)} @things;
 
 foreach my $thing (@things) {
-    say $thing;
+    $thing =~ /walk(\d\d)(\d\d)(\d\d\d\d).*/ || return 0;
+    my $day = $1;
+    my $month = $2;
+    my $year = $3; 
     open(my $fh, '<', $thing) || die "Could not open $thing";
     while (my $row = <$fh>) {
        if($row =~/(\d+\.?\d*)\s+Mile.*/)
        {
-           say $1;
+           my $walk = $1;
+           print "$day\/$month\/$year,";
+           say $walk;
+           $total += $walk;
        }
        
    }
 } 
 closedir $dh;
+
