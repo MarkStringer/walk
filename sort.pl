e68f9d674309b9f5821038f0b8f93542db325b00
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-02-27 16:57:31 +0000

    sort the files by date

---
+++ sort.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+use 5.010;
+
+sub numberOfDays
+{
+    my $thing = pop;
+    my @days = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+    $thing =~ /walk(\d\d)(\d\d)(\d\d\d\d).*/ || return 0;
+    my $day = $1;
+    my $month = $2;
+    my $year = $3;
+    my $daysInPastMonths = 0;
+    foreach my $i (0..$month)
+    {
+       $daysInPastMonths += $days[$i];
+    }
+
+    return $daysInPastMonths + $day + ($year *365);
+} 
+
+my $dir = shift // '.';
+ 
+opendir my $dh, $dir or die "Could not open '$dir' for reading '$!'\n";
+my @things = grep {$_ ne '.' and $_ ne '..' and /^walk.*\.txt$/} readdir $dh;
+
+@things = sort {numberOfDays($a) <=> numberOfDays($b)} @things;
+
+foreach my $thing (@things) {
+    say $thing;
+    open(my $fh, '<', $thing) || die "Could not open $thing";
+    while (my $row = <$fh>) {
+       if($row =~/(\d+\.?\d*)\s+Mile.*/)
+       {
+           say $1;
+       }
+       
+   }
+} 
+closedir $dh;
