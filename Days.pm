c47102f3b22954c9a699207fd72b7865672b770e
Mark Stringer <mark.stringer@mumbly.co.uk>
2017-12-11 12:34:02 +0000

    Some actual test-driven development!

--- Days.pm
+++ Days.pm
@@ -6,28 +6,55 @@ use 5.010;
 
 use Exporter;
 our @ISA    = qw(Exporter);
-our @EXPORT = qw(numberOfDays);
+our @EXPORT = qw(numberOfDays isLeapYear numberOfDaysIncremental);
+my @days = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 sub numberOfDays
 {
     my $thing = pop;
-    my @days = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-
     $thing =~ /walk(\d\d)(\d\d)(\d\d\d\d).*/ || return 0;
-    my $day = $1;
-    my $month = $2;
-    $month = $month -1;
-    my $year = $3;
-    my $daysInPastMonths = 0;
+    numberOfDaysIncremental("01010001", "$1$2$3");
+}
+sub numberOfDaysIncremental
+{
+    my $secondDate = pop;
+    my $firstDate = pop;
+    $firstDate =~ /(\d\d)(\d\d)(\d\d\d\d)/ || return 0;
+    my $firstDay = $1;
+    my $firstMonth = $2;
+    my $firstYear = $3;
+    $secondDate =~ /(\d\d)(\d\d)(\d\d\d\d)/ || return 0;
+    my $secondDay = $1;
+    my $secondMonth = $2;
+    my $secondYear = $3;
 
-    ## don't do this if it's January
-    if ($month > 0)
+    my $counter =0;
+
+    while(($firstDay!=$secondDay) || ($firstMonth!=$secondMonth) || ($firstYear != $secondYear))
     {
-    	foreach my $i (0..$month)
-    	{
-       		$daysInPastMonths += $days[$i];
-    	}
-    }
+        $counter++;
+        $firstDay++;
+        if ($firstDay > ($days[$firstMonth-1] + (($firstMonth==2) && isLeapYear($firstYear))))
+        {
+		$firstDay = 1;
+                $firstMonth++;
+        }
+        if ($firstMonth == 13)
+        {
+	   $firstMonth = 1;
+           $firstYear++;
+        }
+}
+return $counter;
 
-    return $daysInPastMonths + $day + ($year *365);
-} 
+}
+
+sub isLeapYear
+{
+    my $year = pop;
+    !($year %  400) && return 1;
+    !($year %  100) && return 0;
+    !($year %  4) && return 1;
+    return 0;
+}
+
 1;
---
+++ test.pl
@@ -0,0 +1,16 @@
+use Days;
+use Test::Simple tests => 10;
+
+
+ok(numberOfDays("walk22012017.txt")== 736350, "Count Days");
+ok(isLeapYear(2000)==1, "Leap year 2000 true");
+ok(isLeapYear(1996)==1, "Leap Year 1996 true");
+ok(isLeapYear(1997)==0, "Leap Year 1997 false");
+ok(isLeapYear(1900)==0, "Leap Year 1900 false");
+ok(numberOfDaysIncremental("22011969", "23011969")==1, "Test incremental");
+ok(numberOfDaysIncremental("22011969", "22011969")==0, "Test 0 for same day");
+ok(numberOfDaysIncremental("28022000", "01032000")==2, "Test weird Leap year");
+ok(numberOfDaysIncremental("28021996", "28021997")==366, "Test ordinary Leap year");
+ok(numberOfDaysIncremental("28021993", "28021995")==730, "Test ordinary Year");
+
+
