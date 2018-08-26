80be3e27233b186489bdb44eafa4f95b2151da4c
Mark Stringer <mark.stringer@mumbly.co.uk>
2018-08-26 15:25:18 +0100

    Add walks and weight loss

--- sort.pl
+++ sort.pl
@@ -3,34 +3,32 @@ use lib cwd;
 use strict;
 use warnings;
 use 5.010;
-use Days ;
+use Days;
 
-my $dir = shift // '.';
+my $dir   = shift // '.';
 my $total = 0;
 my $delim = "\t";
 
- 
 opendir my $dh, $dir or die "Could not open '$dir' for reading '$!'\n";
-my @things = grep {$_ ne '.' and $_ ne '..' and /^walk.*\.txt$/} readdir $dh;
+my @things = grep { $_ ne '.' and $_ ne '..' and /^walk.*\.txt$/ } readdir $dh;
 
-@things = sort {numberOfDays($a) <=> numberOfDays($b)} @things;
+@things = sort { numberOfDays($a) <=> numberOfDays($b) } @things;
 
 foreach my $thing (@things) {
     $thing =~ /walk(\d\d)(\d\d)(\d\d\d\d).*/ || return 0;
-    my $day = $1;
+    my $day   = $1;
     my $month = $2;
-    my $year = $3; 
-    open(my $fh, '<', $thing) || die "Could not open $thing";
-    while (my $row = <$fh>) {
-       if($row =~/(\d+\.?\d*)\s+Mile.*/i)
-       {
-           my $walk = $1;
-           print "$day\/$month\/$year$delim";
-           print $walk;
-           $total += $walk;
-           say "$delim$total";
-       }
-       
-   }
-} 
+    my $year  = $3;
+    open( my $fh, '<', $thing ) || die "Could not open $thing";
+    while ( my $row = <$fh> ) {
+        if ( $row =~ /(\d+\.?\d*)\s+Mile.*/i ) {
+            my $walk = $1;
+            print "$day\/$month\/$year$delim";
+            print $walk;
+            $total += $walk;
+            say "$delim$total";
+        }
+
+    }
+}
 closedir $dh;
