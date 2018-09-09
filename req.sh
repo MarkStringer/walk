d9a3d0c866eefcc6674a8edc01ebc9cf5fbfbef3
Mark Stringer <mark.stringer@mumbly.co.uk>
2018-09-09 17:44:59 +0100

    Add walks and weight loss

--- req.sh
+++ req.sh
@@ -4,5 +4,5 @@ daysLeft=`calc 365 - $days`
 perl sort.pl > out
 milesWalkedLine=`tail -n 1 out`
 milesWalked=`echo $milesWalkedLine | perl -nle 's/.*\D+(\d+\.\d+)/$1/; print $1'`
-calc 1095 - $milesWalked
+calc "round((1095 - $milesWalked) /$daysLeft,2)"
 
