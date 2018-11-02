14e6d969abb625ea51911ead40a8679b25918e78
Mark Stringer <mark.stringer@mumbly.co.uk>
2018-11-02 23:08:06 +0000

    Add walks and weight loss

--- makegraph.sh
+++ makegraph.sh
@@ -3,9 +3,18 @@ perl sort.pl > out.csv
 perl bestfit.pl out.csv > fit.csv
 `bash weightFit.sh`
 `bash weekWeightFit.sh`
-gnuplot twoaxes.gnu
-firefox file.svg&
+#gnuplot twoaxes.gnu
 
 ## badges and rankings
 ./wagon_badge  7
 ./wagon_badge 14
+./week_rank
+
+## show the graph
+read -n1 -r -p "Show Graph? [Y]/N" key
+if [[ "$key" =~ ^([nN]+$) ]]
+then
+	exit 0
+else
+	firefox file.svg&	
+fi
