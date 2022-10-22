383cc47a0b1e13d8ffaf9aea589f9fcf9c46a115
Mark Stringer <mark.stringerumbly.co.uk>
2022-10-22 16:54:39 +0300

    Add walks and weight loss

---
+++ mgraph.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+perl sort.pl > walk.csv
+tail -n 7 weight.csv > recent.csv
+perl sort.pl | grep Themis > preThemis.csv
+perl processThemis.pl > themis.csv
+gnuplot -e "datafile='weight.csv';recent='recent.csv'" newFormatWlkWt.gnu
+
+# show the graph
+read -n1 -r -p "Show Graph? [Y]/N" key
+if [[ "$key" =~ ^([nN]+$) ]]
+then
+	exit 0
+else
+	cp file.png ../MarkStringer.github.io/assets/ 
+fi
+./commit.sh
+cd ../Mark*
+./commit.sh
+termux-open https://MarkStringer.github.io
