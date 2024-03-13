e98d2c1bdb098231fb03e2269c791dbbd11c9c10
Mark Stringer <mark.stringer@mumbly.co.uk>
2024-03-13 09:40:32 +0000

    Add walks and weight loss

--- makegraph.sh
+++ makegraph.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+# Check if GRAPH_PATH environment variable is set
+if [[ -z "${GRAPH_PATH}" ]]; then
+    read -p "Please enter the path to the SVG file: " GRAPH_PATH
+    export GRAPH_PATH
+fi
+
 perl sort.pl > walk.csv
 tail -n 7 weight.csv > recent.csv
 ##perl sort.pl | grep Themis > preThemis.csv
@@ -6,11 +13,11 @@ tail -n 7 weight.csv > recent.csv
 gnuplot -e "datafile='weight.csv';recent='recent.csv'" newFormatWlkWt.gnu
 
 # show the graph
-read -n1 -r -p "Show Graph? [Y]/N" key
-if [[ "$key" =~ ^([nN]+$) ]]
-then
-	exit 0
+read -n1 -r -p "Show Graph? [Y]/N: " key
+if [[ "$key" =~ ^([nN]+$) ]]; then
+    exit 0
 else
-	ffx 'file:///home/mark/projects/walk/file.svg'&	
+    ffx "file://${GRAPH_PATH}" &
 fi
 ./commit.sh
+
