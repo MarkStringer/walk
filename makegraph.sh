2b56449d33d73b12e472a040085ccf08cd3734f5
Mark Stringer <mark.stringer@mumbly.co.uk>
2025-09-30 12:06:16 +0100

    Add walks and weight loss

--- makegraph.sh
+++ makegraph.sh
@@ -15,7 +15,8 @@ tail -n 7 walk.csv > recentWalk.csv
 ##gnuplot -e "datafile='weight.csv';recent='recent.csv'" newFormatWlkWt.gnu
 ##gnuplot -e "datafile='weight.csv';recent='recent.csv'" newFormatWlkWt_2026.gnu
 ##gnuplot -e "datafile='weight.csv';recent='recent.csv'" newFormatWlkWt_weight_target_coloured.gnu
-gnuplot -e "datafile='weight.csv';recent='recent.csv'; last_weight=117.8; last_date='20250930'"  newFormatWlkWt_weight_target_args.gnu
+today=$(date +%Y%m%d);
+gnuplot -e "datafile='weight.csv';recent='recent.csv'; last_weight=117.8; last_date='${today}'"  newFormatWlkWt_weight_target_args.gnu
 
 
 
